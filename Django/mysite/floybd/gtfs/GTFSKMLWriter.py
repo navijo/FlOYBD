430866739ee95a7dd4ef1db2a741e1d4f08cba9e
Ivan Josa Llovera <navijo@gmail.com>
2017-06-08 15:21:34 +0200

    GTFS
    gtfs

---
+++ Django/mysite/floybd/gtfs/GTFSKMLWriter.py
@@ -0,0 +1,255 @@
+import xml.etree.ElementTree as ET
+from ..gtfs_models import Trip
+
+def init():
+    root = ET.Element('kml')
+    root.attrib['xmlns'] = 'http://earth.google.com/kml/2.1'
+    doc = ET.SubElement(root, 'Document')
+    open_tag = ET.SubElement(doc, 'open')
+    open_tag.text = '1'
+
+    return doc
+
+def end(doc,name):
+    output = open("static/kmls/"+name+".kml", 'wb')
+    output.write(b"""<?xml version="1.0" encoding="UTF-8"?>\n""")
+    ET.ElementTree(doc).write(output, 'utf-8')
+
+
+def CreateFolder(parent, name, visible=True, description=None):
+    folder = ET.SubElement(parent, 'Folder')
+    name_tag = ET.SubElement(folder, 'name')
+    name_tag.text = name
+    if description is not None:
+      desc_tag = ET.SubElement(folder, 'description')
+      desc_tag.text = description
+    if not visible:
+      visibility = ET.SubElement(folder, 'visibility')
+      visibility.text = '0'
+    return folder
+
+
+def StopFolderSelectionMethod(stop_folder):
+
+    # Create the various sub-folders for showing the stop hierarchy
+    station_folder = CreateFolder(stop_folder, 'Stations')
+    platform_folder = CreateFolder(stop_folder, 'Platforms')
+    platform_connections = CreateFolder(platform_folder, 'Connections')
+    entrance_folder = CreateFolder(stop_folder, 'Entrances')
+    entrance_connections = CreateFolder(entrance_folder, 'Connections')
+    standalone_folder = CreateFolder(stop_folder, 'Stand-Alone')
+
+    def FolderSelectionMethod(stop):
+      return (standalone_folder, None)
+    return FolderSelectionMethod
+
+
+def SetIndentation(elem, level=0):
+    """Indented the ElementTree DOM.
+
+    This is the recommended way to cause an ElementTree DOM to be
+    prettyprinted on output, as per: http://effbot.org/zone/element-lib.htm
+
+    Run this on the root element before outputting the tree.
+
+    Args:
+      elem: The element to start indenting from, usually the document root.
+      level: Current indentation level for recursion.
+    """
+    i = "\n" + level*"  "
+    if len(elem):
+      if not elem.text or not elem.text.strip():
+        elem.text = i + "  "
+      for elem in elem:
+        SetIndentation(elem, level+1)
+      if not elem.tail or not elem.tail.strip():
+        elem.tail = i
+    else:
+      if level and (not elem.tail or not elem.tail.strip()):
+        elem.tail = i
+
+def CreateStyle(doc, style_id, style_dict):
+    def CreateElements(current_element, current_dict):
+      for (key, value) in current_dict.items():
+        element = ET.SubElement(current_element, key)
+        if isinstance(value, dict):
+          CreateElements(element, value)
+        else:
+          element.text = value
+
+    style = ET.SubElement(doc, 'Style', {'id': style_id})
+    CreateElements(style, style_dict)
+    return style
+
+def StopStyleSelectionMethod(doc):
+
+    CreateStyle(doc, 'stop_standalone', {'IconStyle': {'color': 'ff00ff00'}})
+
+    def StyleSelectionMethod(stop):
+      return ('stop_standalone', None)
+
+    return StyleSelectionMethod
+
+
+def CreatePlacemark(parent, name, style_id=None, visible=True,description=None):
+
+    placemark = ET.SubElement(parent, 'Placemark')
+    placemark_name = ET.SubElement(placemark, 'name')
+    placemark_name.text = name
+    if description is not None:
+      desc_tag = ET.SubElement(placemark, 'description')
+      desc_tag.text = description
+    if style_id is not None:
+      styleurl = ET.SubElement(placemark, 'styleUrl')
+      styleurl.text = '#%s' % style_id
+    if not visible:
+      visibility = ET.SubElement(placemark, 'visibility')
+      visibility.text = '0'
+    return placemark
+
+
+def CreateStopPlacemark(stop_folder, stop, style_id):
+    desc_items = []
+    desc_items.append("Stop id: %s" % stop.stop_id)
+    if stop.stop_desc:
+      desc_items.append(stop.stop_desc)
+    if stop.stop_url:
+      desc_items.append('Stop info page: <a href="%s">%s</a>' % (
+          stop.stop_url, stop.stop_url))
+    description = '<br/>'.join(desc_items) or None
+    placemark = CreatePlacemark(stop_folder, stop.stop_name,
+                                        description=description,
+                                        style_id=style_id)
+    point = ET.SubElement(placemark, 'Point')
+    coordinates = ET.SubElement(point, 'coordinates')
+    coordinates.text = '%.6f,%.6f' % (stop.stop_lon, stop.stop_lat)
+
+
+def CreateStyleForRoute( doc, route):
+    style_id = 'route_%s' % route.route_id
+    style = ET.SubElement(doc, 'Style', {'id': style_id})
+    linestyle = ET.SubElement(style, 'LineStyle')
+    width = ET.SubElement(linestyle, 'width')
+    type_to_width = {0: '3',  # Tram
+                     1: '3',  # Subway
+                     2: '5',  # Rail
+                     3: '1'}  # Bus
+    width.text = type_to_width.get(route.route_type, '1')
+    if route.route_color:
+      color = ET.SubElement(linestyle, 'color')
+      red = route.route_color[0:2].lower()
+      green = route.route_color[2:4].lower()
+      blue = route.route_color[4:6].lower()
+      color.text = 'ff%s%s%s' % (blue, green, red)
+    return style_id
+
+
+def CreateLineString(parent, coordinate_list):
+
+    if not coordinate_list:
+      return None
+    linestring = ET.SubElement(parent, 'LineString')
+    tessellate = ET.SubElement(linestring, 'tessellate')
+    tessellate.text = '1'
+    if len(coordinate_list[0]) == 3:
+      altitude_mode = ET.SubElement(linestring, 'altitudeMode')
+      altitude_mode.text = 'absolute'
+    coordinates = ET.SubElement(linestring, 'coordinates')
+    if len(coordinate_list[0]) == 3:
+      coordinate_str_list = ['%f,%f,%f' % t for t in coordinate_list]
+    else:
+      coordinate_str_list = ['%f,%f' % t for t in coordinate_list]
+    coordinates.text = ' '.join(coordinate_str_list)
+    return linestring
+
+
+def CreateRoutePatternsFolder(parent, route, style_id=None, visible=True):
+    #trips = Trip.objects.filter(route_id=route.route_id)
+
+    pattern_id_to_trips = route.GetPatternIdTripDict()
+    #if not pattern_id_to_trips:
+     # return None
+
+    # sort by number of trips using the pattern
+    pattern_trips = pattern_id_to_trips.values()
+    pattern_trips.sort(lambda a, b: (len(a) > len(b))-(len(a) < len(b)))
+
+    folder = CreateFolder(parent, 'Patterns', visible)
+    #for n, trips in enumerate(pattern_trips):
+    for n, trips in enumerate(pattern_trips):
+      trip_ids = [trip.trip_id for trip in trips]
+      name = 'Pattern %d (trips: %d)' % (n+1, len(trips))
+      description = 'Trips using this pattern (%d in total): %s' % (
+          len(trips), ', '.join(trip_ids))
+      placemark = CreatePlacemark(folder, name, style_id, visible,description)
+      coordinates = [(stop.stop_lon, stop.stop_lat)
+                     for stop in trips[0].GetPattern()]
+      CreateLineString(placemark, coordinates)
+    return folder
+
+
+
+def createStopsFolder(doc, stops):
+    stopsFolder = CreateFolder(doc, "Stops")
+    stop_folder_selection = StopFolderSelectionMethod(stopsFolder)
+    stop_style_selection = StopStyleSelectionMethod(doc)
+    for stop in stops:
+        (folder, pathway_folder) = stop_folder_selection(stop)
+        (style_id, pathway_style_id) = stop_style_selection(stop)
+        CreateStopPlacemark(folder, stop, style_id)
+    return stopsFolder
+
+
+def CreateRoutesFolder(doc, routes, route_type=None):
+
+    def GetRouteName(route):
+      name_parts = []
+      if route.route_short_name:
+        name_parts.append('<b>%s</b>' % route.route_short_name)
+      if route.route_long_name:
+        name_parts.append(route.route_long_name)
+      return ' - '.join(name_parts) or route.route_id
+
+    def GetRouteDescription(route):
+      desc_items = []
+      if route.route_desc:
+        desc_items.append(route.route_desc)
+      if route.route_url:
+        desc_items.append('Route info page: <a href="%s">%s</a>' % (
+            route.route_url, route.route_url))
+      description = '<br/>'.join(desc_items)
+      return description or None
+
+    routes = [route for route in routes
+              if route_type is None or route.route_type == route_type]
+    if not routes:
+      return None
+    routes.sort(key=lambda x: GetRouteName(x))
+
+    if route_type is not None:
+      route_type_names = {0: 'Tram, Streetcar or Light rail',
+                          1: 'Subway or Metro',
+                          2: 'Rail',
+                          3: 'Bus',
+                          4: 'Ferry',
+                          5: 'Cable car',
+                          6: 'Gondola or suspended cable car',
+                          7: 'Funicular'}
+      type_name = route_type_names.get(route_type, str(route_type))
+      folder_name = 'Routes - %s' % type_name
+    else:
+      folder_name = 'Routes'
+    routes_folder = CreateFolder(doc, folder_name, visible=False)
+
+    for route in routes:
+      style_id = CreateStyleForRoute(doc, route)
+      route_folder = CreateFolder(routes_folder, GetRouteName(route),
+                                        description=GetRouteDescription(route))
+      #CreateRouteShapesFolder(route., route_folder, route,style_id, False)
+      CreateRoutePatternsFolder(route_folder, route, style_id, False)
+
+    return routes_folder
+
+
+
+
--- Django/mysite/floybd/gtfs/viewsGTFS.py
+++ Django/mysite/floybd/gtfs/viewsGTFS.py
@@ -5,12 +5,14 @@ from ..forms import UploadFileForm
 import simplekml
 
 import os
+import shutil
 import tarfile
 import zipfile
 import time
 from ..utils import *
 from .GTFSUtils import *
 from ..gtfs_models import Agency
+from .GTFSKMLWriter import *
 
 def uploadGTFS(request):
     if request.method == 'POST':
@@ -59,6 +61,7 @@ def parseGTFS(title):
 
     parseAgency("static/upload/gtfs/"+title)
     parseCalendar("static/upload/gtfs/"+title)
+    parseCalendarDates("static/upload/gtfs/" + title)
     parseStops("static/upload/gtfs/" + title)
     parseRoutes("static/upload/gtfs/" + title)
     parseTrips("static/upload/gtfs/" + title)
@@ -131,32 +134,216 @@ def viewgtfs(request):
     return render(request, 'floybd/gtfs/viewGTFSAgencies.html', {'agencies': agencies})
 
 
-def getAgenciesAndGenerateKML(request):
+def getAgenciesAndGenerateKML1(request):
     agencies = request.POST.getlist('agenciesSelected')
+
     gtfsKml = simplekml.Kml()
     stops_folder = gtfsKml.newfolder(name='Stops')
     routes_folder = gtfsKml.newfolder(name='Routes')
     for selectedAgency in agencies:
+        print("selectedAgency:" + str(selectedAgency))
         agency = Agency.objects.get(agency_id=selectedAgency)
+        print("Agency:" + str(agency.agency_name))
         #getRoutesForAgency
-        routes = Route.objects.filter(agency_id=agency.agency_id)
+        routes = Route.objects.filter(agency=agency)
         for route in routes:
             print("Route:" + str(route.route_long_name))
             # getTripForRoute
             trips = Trip.objects.filter(route_id=route.route_id)
             for trip in trips:
+                print("Trip: " + str(trip.trip_id) + "\t" + str(trip.trip_headsign))
                 #getCalendarForTrip
                 calendar = Calendar.objects.filter(service_id=trip.service_id)
                 #getCalendarInfo
                 # getStopTimesForTrip
                 stop_times = Stop_time.objects.filter(trip_id=trip.trip_id)
                 for stop_time in stop_times:
-                    stops = Stop.objects.filter(stop_id=stop_time.stop_id)
-                    for stop in stops:
-                        stops_folder.newpoint(name=stop.name, description=stop.stop_desc,
+                    print("Stop Time: " +
+                          str(stop_time.arrival_time) +
+                          "\t" +
+                          str(stop_time.departure_time) +
+                          "\t" +
+                          str(stop_time.stop_id) +
+                          "\n")
+                    stop = stop_time.stop
+                    print("Stop: " + str(stop.stop_name))
+                   # for stop in stops:
+                     #   print("Stop: " + str(stop.name))
+                    stops_folder.newpoint(name=stop.stop_name, description=stop.stop_desc,
                                               coords=[(stop.stop_lon, stop.stop_lat)])
+
 
     kmlPath = "static/kmls/gtfs.kml"
     gtfsKml.save(kmlPath)
 
-    return kmlPath
+    agencies = Agency.objects.all()
+    return render(request, 'floybd/gtfs/viewGTFSAgencies.html', {'agencies': agencies})
+    #return kmlPath
+
+
+
+def getAgenciesAndGenerateKML2(request):
+    agencies = request.POST.getlist('agenciesSelected')
+
+    doc = init()
+
+    for selectedAgency in agencies:
+        stopsList = []
+        routesList = []
+        print("selectedAgency:" + str(selectedAgency))
+        agency = Agency.objects.get(agency_id=selectedAgency)
+        print("Agency:" + str(agency.agency_name))
+        routes = Route.objects.filter(agency=agency)
+        for route in routes:
+            print("Route:" + str(route.route_long_name))
+            routesList.append(route)
+            trips = Trip.objects.filter(route_id=route.route_id)
+            print(type(trips))
+            for trip in trips:
+                print("Trip: " + str(trip.trip_id) + "\t" + str(trip.trip_headsign))
+                stop_times = Stop_time.objects.filter(trip_id=trip.trip_id)
+                for stop_time in stop_times:
+                    stop = stop_time.stop
+                    stopsList.append(stop)
+                    print("Stop: " + str(stop.stop_name))
+
+        createStopsFolder(doc, stopsList)
+        CreateRoutesFolder(doc, routesList)
+
+    SetIndentation(doc)
+    end(doc, "gtfs")
+
+
+    kmlPath = "static/kmls/gtfs.kml"
+
+
+    agencies = Agency.objects.all()
+    return render(request, 'floybd/gtfs/viewGTFSAgencies.html', {'agencies': agencies})
+
+
+def getAgenciesAndGenerateKML(request):
+    agencies = request.POST.getlist('agenciesSelected')
+
+    calendars_added = []
+    stops_added = []
+
+    millis = int(round(time.time() * 1000))
+    folderName = "static/kmls/"+str(millis)
+    os.mkdir(folderName)
+
+    agencies_file = open(folderName+"/agency.txt", 'w')
+    agencies_file.write("agency_url,agency_name,agency_id,agency_timezone\n")
+
+    calendar_file = open(folderName + "/calendar.txt", 'w')
+    calendar_file.write("service_id,start_date,end_date,monday,tuesday,wednesday,thursday,friday,saturday,sunday\n")
+
+    routes_file = open(folderName + "/routes.txt", 'w')
+    routes_file.write("route_type,route_id,route_short_name,route_long_name,agency_id\n")
+
+    stops_file = open(folderName + "/stops.txt", 'w')
+    stops_file.write("stop_lon,stop_name,stop_lat,stop_id,location_type\n")
+
+    stops_times_file = open(folderName + "/stop_times.txt", 'w')
+    stops_times_file.write("trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,"
+                           "drop_off_type,shape_dist_traveled\n")
+
+    trips_file = open(folderName + "/trips.txt", 'w')
+    trips_file.write("route_id,trip_id,trip_headsign,service_id\n")
+
+    for selectedAgency in agencies:
+        agency = Agency.objects.get(agency_id=selectedAgency)
+        agencyStr = str(agency.agency_url) +\
+                    "," + str(agency.agency_name) + \
+                    "," + str(agency.agency_id) + \
+                    "," + str(agency.agency_timezone)+"\n"
+        agencies_file.write(agencyStr)
+
+        routes = Route.objects.filter(agency=agency)
+        for route in routes:
+
+            routeStr = str(route.route_type) + "," + str(route.route_id) + \
+                        "," + str(route.route_short_name) + \
+                        "," + str(route.route_long_name) + \
+                        "," + str(route.agency_id)+"\n"
+
+            routes_file.write(routeStr)
+
+            trips = Trip.objects.filter(route_id=route.route_id)
+
+            for trip in trips:
+                tripStr = str(trip.route_id) + "," + str(trip.trip_id) + \
+                           "," + str(trip.trip_headsign) + \
+                           "," + str(trip.service_id)+"\n"
+
+                trips_file.write(tripStr)
+
+                if trip.service not in calendars_added:
+                    calendars_added.append(trip.service)
+                    calendar = Calendar.objects.get(service_id=trip.service)
+
+                    calendarStr = str(calendar.service_id) +\
+                                      "," + str(calendar.start_date.strftime("%Y%m%d")) + \
+                                      "," + str(calendar.end_date.strftime("%Y%m%d")) + \
+                                      (str(",1") if calendar.monday else (str(",0"))) + \
+                                      (str(",1") if calendar.tuesday else (str(",0"))) + \
+                                      (str(",1") if calendar.wednesday else (str(",0"))) + \
+                                      (str(",1") if calendar.thursday else (str(",0"))) + \
+                                      (str(",1") if calendar.friday else (str(",0"))) + \
+                                      (str(",1") if calendar.saturday else (str(",0"))) + \
+                                      (str(",1") if calendar.sunday else (str(",0"))) +\
+                                        str("\n")
+                    calendar_file.write(calendarStr)
+
+                stop_times = Stop_time.objects.filter(trip_id=trip.trip_id)
+                for stop_time in stop_times:
+                    stop_times_str = str(stop_time.trip.trip_id) +\
+                                      "," + str(stop_time.arrival_time) + \
+                                      "," + str(stop_time.departure_time) + \
+                                      "," + str(stop_time.stop_id) + \
+                                      "," + str(stop_time.stop_sequence) + \
+                                      "," + str(stop_time.stop_headsign) + \
+                                      "," + str(stop_time.pickup_type) + \
+                                      "," + str(stop_time.drop_off_type)+ \
+                                      "," + str("0")+"\n"
+
+                    stops_times_file.write(stop_times_str)
+
+                    stop = stop_time.stop
+                    if stop.stop_id not in stops_added:
+                        print("Added Stop:" +stop.stop_id )
+                        stops_added.append(stop.stop_id)
+
+        for stop_id in stops_added:
+            stop = Stop.objects.get(stop_id=stop_id)
+            stopStr = str(stop.stop_lon) + \
+                      "," + str(stop.stop_name) + \
+                      "," +str(stop.stop_lat) + \
+                      "," +str(stop.stop_id) + \
+                      "," +str(stop.location_type)+"\n"
+            stops_file.write(stopStr)
+
+    agencies_file.close()
+    calendar_file.close()
+    routes_file.close()
+    stops_file.close()
+    stops_times_file.close()
+    trips_file.close()
+
+    shutil.make_archive(folderName, 'zip', folderName)
+
+    zipName = folderName+".zip"
+    kmlName = str(millis)+".kml"
+
+    command1 = "python2 static/utils/gtfs/kmlwriter.py "+zipName+" static/kmls/"+kmlName
+    os.system(command1)
+
+    ip = getIp()
+
+    command = "echo 'http://" + ip + ":8000/static/kmls/" + kmlName + \
+              "' | sshpass -p lqgalaxy ssh lg@192.168.88.198 'cat - > /var/www/html/kmls.txt'"
+    os.system(command)
+
+    agencies = Agency.objects.all()
+    return render(request, 'floybd/gtfs/viewGTFSAgencies.html', {'agencies': agencies})
+
+
