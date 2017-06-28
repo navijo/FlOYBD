558c5879e1952163b5eea59a6de5f38838037fb7
Ivan Josa Llovera <navijo@gmail.com>
2017-06-28 12:24:51 +0200

    code styling

--- DataGatheringAndCleaning/gatherEarthquakes.py
+++ DataGatheringAndCleaning/gatherEarthquakes.py
@@ -12,6 +12,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 base_url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
 
+
 def writeFile(fileName, data):
     """Write the information to a file"""
     print("###Writing file")
@@ -33,78 +34,43 @@ def insertIntoDB(data):
     magnitude = properties["mag"]
     time = properties["time"]
     place = properties["place"]
-    fecha =  datetime.fromtimestamp(float(properties["time"])/1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    fecha = datetime.fromtimestamp(float(properties["time"])/1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
     geoJson = str(data)
 
-    magnitude = float(str(magnitude).replace(",",".")) if not (magnitude==None) else None
-    depth = float(str(depth).replace(",",".")) if not (depth==None) else None
-
+    magnitude = float(str(magnitude).replace(",", ".")) if not (magnitude is None) else None
+    depth = float(str(depth).replace(",", ".")) if not (depth is None) else None
 
     if str(place) and str(time):
         session.execute("INSERT INTO Earthquake (\"eventId\",\"place\",\"time\",\"fecha\",\"magnitude\",\"depth\",\"longitude\",\"latitude\",\"geojson\") \
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s,%s)",
-        [str(eventId),str(place), str(time),fecha,magnitude, depth, longitude, latitude,geoJson])
+        [str(eventId), str(place), str(time), fecha, magnitude, depth, longitude, latitude, geoJson])
 
-
-def getEarthQuakesSpain(yearFrom):
-
-    spainMinLatitude = 36
-    spainMaxLatitude = 44
-    spainMinLongitude = -9
-    spainMaxLongitude = 4
-
-    startDateStr = str(yearFrom)+"-01-01"
-    url = "&starttime="+startDateStr+"&minlatitude=36&maxlatitude=44&minlongitude=-9&maxlongitude=4"
-    printLog("Calculating from "+str(yearFrom))
-
-    response = requests.get(base_url+url)
-    jsonData = response.json()
-    for feature in jsonData.get("features"):
-        properties = feature.get("properties")
-        dateTime =  datetime.datetime.fromtimestamp(float(properties["time"])/1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        printLog("Processing :" + properties["place"]+" for: " + str(dateTime))
-        insertIntoDB(feature)
-
-    # Canarias
-    canariasMinLatitude = 27
-    canariasMaxLatitude = 30
-    canariasMinLongitude = -18
-    canariasMaxLongitude = -13
-    url = "&starttime="+startDateStr+"&minlatitude=27&maxlatitude=30&minlongitude=-18&maxlongitude=-13"
-    printLog("Calculating Canarias from "+str(yearFrom))
-
-    response = requests.get(base_url+url)
-    jsonData = response.json()
-    for feature in jsonData.get("features"):
-        properties = feature.get("properties")
-        dateTime =  datetime.fromtimestamp(float(properties["time"])/1000).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        printLog("Processing :" + properties["place"]+" for: " + str(dateTime))
-        insertIntoDB(feature)
 
 def getEarthQuakesWorld(yearFrom):
 
     now = datetime.now()
-    while(yearFrom<=now.year):
+    while yearFrom <= now.year:
         startDateStr = str(yearFrom)+"-01-01"
         endDateStr = str(yearFrom)+"-12-31"
 
         try:
             getData(startDateStr,endDateStr)
-        except (simplejson.scanner.JSONDecodeError):
+        except simplejson.scanner.JSONDecodeError:
             printLog("####ERROR!!!"+str(yearFrom))
             getDataMonthly(yearFrom)
         finally:
             yearFrom = yearFrom+1
 
+
 def getDataMonthly(yearFrom):
-    for i in range(1,12):
+    for i in range(1, 12):
         startDateStr = str(yearFrom)+"-"+str(i)+"-01"
         endDateStr = str(yearFrom)+"-"+str((i+1)%13)+"-01"
-        getData(startDateStr,endDateStr)
+        getData(startDateStr, endDateStr)
         time.sleep(10)
 
 
-def getData(fromDate,toDate):
+def getData(fromDate, toDate):
     url = "&starttime="+fromDate+"&endtime="+toDate
     printLog("Calculating from "+str(fromDate)+" to " + str(toDate))
   
@@ -116,7 +82,7 @@ def getData(fromDate,toDate):
         try:
             printLog("Processing :" + str(properties["place"])+" for: " + str(dateTime))
             insertIntoDB(feature)
-        except(UnicodeEncodeError):
+        except UnicodeEncodeError:
             printLog("Bad encoding")
 
 
@@ -125,6 +91,5 @@ def getData(fromDate,toDate):
 if __name__ == "__main__":
     cluster = Cluster(['192.168.246.236'])
     session = cluster.connect("dev")
-    #getEarthQuakesSpain(1930)
     getEarthQuakesWorld(2017)
     printLog("END")
