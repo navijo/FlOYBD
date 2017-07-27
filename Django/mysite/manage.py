a3e49ef978263d2b79ca7c8b8609d0c7fb0d7f85
Ivan Josa Llovera <navijo@gmail.com>
2017-07-27 15:28:31 +0200

    manage.py update

--- Django/mysite/manage.py
+++ Django/mysite/manage.py
@@ -5,12 +5,13 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-    import django
 
-    django.setup()
-    from floybd.models import Setting
     try:
         from django.core.management import execute_from_command_line
+        import django
+
+        django.setup()
+        from floybd.models import Setting
     except ImportError:
         try:
             import django
