7faed02de7cba227b70962d43b4e445e6ab973e8
Ivan Josa Llovera <navijo@gmail.com>
2017-08-23 20:39:16 +0200

    Update cronDjangoTask.sh

--- cronDjangoTask.sh
+++ cronDjangoTask.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source ~/FlOYBD/virtualEnv/bin/activate
-cd ~/FlOYBD/Django/mysite/
+source virtualEnv/bin/activate
+cd Django/mysite/
 nohup python manage.py getCurrentWeather &> logCurrentWeatherJob.out &
 nohup python manage.py getLastWeekEarthquakes &> loglastWeekEarthquakes.out &
 nohup python manage.py getLastWeekEarthquakesHeatMap &> logLastWeekEarthquakesHeatMap.out &
